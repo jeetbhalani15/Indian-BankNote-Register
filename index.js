42b8b38a740b50f0c2b57a9b584bc567d23c50d4
jeetbhalani <jeetbhalani@gmail.com>
2021-08-21 21:15:13 +0530

    intial setup 1

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const billAmt = document.querySelector("#bill-amt");
-const cashAmt = document.querySelector("#cash-amt");
+const billAmt = document.querySelector("#bill-amnt");
+const cashAmt = document.querySelector("#cash-amnt");
 const btn = document.querySelector("#btn");
 const checkBtn = document.querySelector("#checkBtn");
 const hiddenCont = document.querySelector(".hidden-cont");
 const errorMsgTxt = document.querySelector("#error-msg");
 const cashTable = document.querySelector("#cash-table");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 btn.addEventListener('click', () => {
-    hideError();
+    HideError();
+    console.log("billAmt")
     if (Number(billAmt.value) > 0) {
         btn.style.display = "none";
         hiddenCont.style.display = "flex";
-    }
-    else {
+    }else {
         ShowError("Enter valid Bill Amount");
     }
 });
+
 checkBtn.addEventListener('click', () => {
-    hideError();
+    HideError();
     if (Number(cashAmt.value) > 0) {
         if (Number(cashAmt.value) >= Number(billAmt.value)) {
             const amountToBeReturn = Number(cashAmt.value) - Number(billAmt.value);
             calculateReturnMoney(amountToBeReturn);
             cashTable.style.display = "flex";
-        }
-
-        else {
+        } else {
             ShowError("kitchen is that side!!")
             cashTable.style.display = "none";
         }
-    }
-    else {
+    } else {
         ShowError(" The cash provided should  be equal to the bill amount");
         cashTable.style.display = "none";
     }
 });
+
 function calculateReturnMoney(amountToBeReturn) {
     for (let i = 0; i < availNotes.length; i++) {
-        const numberOFNote = math.trunc(amountToBeReturn / availNotes[i]);
+        const numberOFNote = Math.trunc(amountToBeReturn / availNotes[i]);
         amountToBeReturn = amountToBeReturn - numberOFNote * availNotes[i];
         noOfNotes[i].innerText = numberOFNote;
 
@@ -51,7 +50,7 @@ function ShowError(msg) {
     errorMsgTxt.style.display = "block"
     errorMsgTxt.innerText = msg
 }
-function hideError() {
+function HideError() {
     errorMsgTxt.style.display = "none"
 };
 
